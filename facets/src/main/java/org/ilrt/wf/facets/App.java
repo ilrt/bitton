3d3ade887764bc483307ea9fd9e78c36d188025b
Mike Jones <mike.a.jones@bristol.ac.uk>
2010-02-16 09:43:05 +0000

    Initial commit

---
+++ facets/src/main/java/org/ilrt/wf/facets/App.java
@@ -0,0 +1,13 @@
+package org.ilrt.wf.facets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
