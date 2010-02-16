aa8603a8fa8754fd6bd3475e629d6410e93fee3a
Mike Jones <mike.a.jones@bristol.ac.uk>
2010-02-16 14:33:20 +0000

    added very genric types that will probably be removed :-)

--- facets/src/main/java/org/ilrt/wf/facets/FacetQueryService.java
+++ facets/src/main/java/org/ilrt/wf/facets/FacetQueryService.java
@@ -5,6 +5,8 @@ package org.ilrt.wf.facets;
  * @author Mike Jones (mike.a.jones@bristol.ac.uk)
  * @author Damian Steer (d.steer.@bristol.ac.uk)
  */
-public interface FacetQueryService {
+public interface FacetQueryService<T,E> {
+
+    T query(E e);
 
 }
