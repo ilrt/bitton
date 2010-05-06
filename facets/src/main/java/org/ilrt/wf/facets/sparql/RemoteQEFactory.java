dcea264b4e6380056be954a606cd845013feaa92
Damian Steer <pldms@Colossus.local>
2010-05-06 11:38:39 +0100

    Added debug logging

--- facets/src/main/java/org/ilrt/wf/facets/sparql/RemoteQEFactory.java
+++ facets/src/main/java/org/ilrt/wf/facets/sparql/RemoteQEFactory.java
@@ -8,12 +8,15 @@ package org.ilrt.wf.facets.sparql;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author pldms
  */
 public class RemoteQEFactory implements QEFactory {
+    private final static Logger log = LoggerFactory.getLogger(RemoteQEFactory.class);
     private final String endpoint;
 
     public RemoteQEFactory(String endpoint) {
@@ -22,6 +25,7 @@ public class RemoteQEFactory implements QEFactory {
 
     @Override
     public QueryExecution get(Query query) {
+        log.debug("Query issued: {}", query);
         return QueryExecutionFactory.sparqlService(endpoint, query);
     }
 
