5bc9ae0f3435b7ba4d4b887b6281b6c1803cd26c
Chris Bailey <c.bailey@bristol.ac.uk>
2011-04-06 18:06:16 +0100

    refactoring and commenting/removing unneeded code

--- facets-spring3-freemarker/src/main/webapp/scripts/script.js
+++ facets-spring3-freemarker/src/main/webapp/scripts/script.js
@@ -2,6 +2,90 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+var slider_uID = 1;
+
+$(document).ready(function(){ 
+	initLoadingIndicator();
+	
+	applyTabs();
+
+	renderImpacts();
+
+	shrink($('#pubtype-facet ul'),5);
+	shrink($('#pubyear-facet ul'),5);
+	shrink($('#home-publist'),5);
+
+	$('#people-complete').
+		autocomplete('http://resrev.ilrt.bris.ac.uk/Completor/resources/complete/a/person').
+		result(function(event, data, formatted) {
+				if (data) $('#people-complete-target').val(data[1]);
+				else $('#people-complete-target').find('input.target').val('');
+		});
+	$('#dept-complete').
+		autocomplete('http://resrev.ilrt.bris.ac.uk/Completor/resources/complete/a/org').
+		result(function(event, data, formatted) {
+				if (data) $('#dept-complete-target').val(data[1]);
+				else $('#dept-complete-target').find('input.target').val('');
+		});
+
+	//Put in placeholders for browsers that don't do it natively:
+	$( "input, textarea" ).placeholder();
+
+	// convert select lists to autocomplete
+	$('select.autocomplete').select_autocomplete();
+	
+	// catch case where select is converted to autoselect and no department has been selected
+	$('#pdept').parent().submit(function(){
+		if ($('#pdept').val() == '')
+			{
+				alert('No department selected');
+				return false; 
+			}
+	});
+	
+	hideDepartments();
+
+//	var link = "[<span>+</span>]&nbsp;";
+//	if ($(".collapsible .content:hidden").length > 0)
+//	{
+//		$(".collapsible .heading").prepend("[<span class='indicator'>+</span>]&nbsp;");
+//	}
+//	else
+//	{
+//		$(".collapsible .heading").prepend("[<span class='indicator'>-</span>]&nbsp;");
+//	}
+//	$(".collapsible .heading").click(function(){
+//			var indicatorText = $(".collapsible .indicator").text();
+//			indicatorText = indicatorText == '+' ? '-' : '+';
+//			$(".collapsible .indicator").text(indicatorText);
+//			$(this).parent().children(".content").slideToggle().removeClass("collapsed");
+//	});
+
+//	$(".autoselect").each(function(){
+//		var title = $(this).attr("title");
+//		var submit = $(this).attr("submit");
+//		var resource = $(this).attr("resource");
+//		var url = COMPLETOR_URL + '?title='+title + '&submit=' + submit + '&resource=' + resource;
+//		$(this).load(encodeURI(url), function(response, status, xhr) {
+//			if (status != 'success' || xhr.status == '404')
+//			{
+//				handleLoadError(this);
+//			}
+//			else
+//			{
+//				try
+//				{
+//					// call any initalisation functions
+//					init();
+//				}
+//				catch (err)
+//				{
+//					handleLoadError(this);
+//				}
+//			}
+//		});
+//	});
+}); // END $(document).ready(function(){...});
 
 function shrink(list,n) {
 	/* Takes a list and hides elements after the nth.
@@ -36,205 +120,74 @@ function shrink(list,n) {
 			return false;
 		});
 	}
-
-}
-
-$(document).ready(function(){ 
-    $('#loading').hide();
-    
-    
-    window.onbeforeunload = function()  {
-        $('#loading').delay(2000).show(0);
-
-        // allow user to cancel popup window
-        $(document).keyup(function(e)
-        {
-            if (e.keyCode == 27) { $('#loading').hide(); }   // esc
-        });
-    };
-    
-        applyTabs();
-
-        renderImpacts();
-
-        shrink($('#pubtype-facet ul'),5);
-        shrink($('#pubyear-facet ul'),5);
-        shrink($('#home-publist'),5);
-        
-        $('#people-complete').
-            autocomplete('http://resrev.ilrt.bris.ac.uk/Completor/resources/complete/a/person').
-            result(function(event, data, formatted) {
-                    if (data) $('#people-complete-target').val(data[1]);
-                    else $('#people-complete-target').find('input.target').val('');
-            });
-        $('#dept-complete').
-            autocomplete('http://resrev.ilrt.bris.ac.uk/Completor/resources/complete/a/org').
-            result(function(event, data, formatted) {
-                    if (data) $('#dept-complete-target').val(data[1]);
-                    else $('#dept-complete-target').find('input.target').val('');
-            });
-}); 
-
-var slider_uID = 1;
-
-$(document).ready(function(){
-    //Put in placeholders for browsers that don't do it natively:
-    $( "input, textarea" ).placeholder();
-
-    $('select.autocomplete').select_autocomplete();
-    $('#pdept').parent().submit(function(){
-        if ($('#pdept').val() == '')
-            {
-                alert('No department selected');
-                return false; 
-            }
-    });
-    
-    hideDepartments();
-
-    var link = "[<span>+</span>]&nbsp;";
-    if ($(".collapsible .content:hidden").length > 0)
-    {
-        $(".collapsible .heading").prepend("[<span class='indicator'>+</span>]&nbsp;");
-    }
-    else
-    {
-        $(".collapsible .heading").prepend("[<span class='indicator'>-</span>]&nbsp;");
-    }
-    $(".collapsible .heading").click(function(){
-
-            var indicatorText = $(".collapsible .indicator").text();
-            indicatorText = indicatorText == '+' ? '-' : '+';
-            $(".collapsible .indicator").text(indicatorText);
-
-            $(this).parent().children(".content").slideToggle().removeClass("collapsed");
-
-    });
-
-    $(".autoselect").each(function(){
-        var title = $(this).attr("title");
-        var submit = $(this).attr("submit");
-        var resource = $(this).attr("resource");
-        var url = COMPLETOR_URL + '?title='+title + '&submit=' + submit + '&resource=' + resource;
-        $(this).load(encodeURI(url), function(response, status, xhr) {
-            if (status != 'success' || xhr.status == '404')
-            {
-                handleLoadError(this);
-            }
-            else
-            {
-                try
-                {
-                    // call any initalisation functions
-                    init();
-                }
-                catch (err)
-                {
-                    handleLoadError(this);
-                }
-            }
-        });
-    });
-
-    // enhance date range selectors
-    if ($("h4:contains(Start Year)").parent().find("li").length > 1)
-    {
-        var dateFacet = $("h4:contains(Start Year)").parent();
-        enableSlider(dateFacet);
-    }
-    if ($("h4:contains(End Year)").parent().find("li").length > 1)
-    {
-        var dateFacet = $("h4:contains(End Year)").parent();
-
-        enableSlider(dateFacet);  
-    }
-});
-
-function enableSlider(div)
-{
-        var min = $("li:first a", div).text();
-        var max =$("li:last a", div).text();
-
-	var ticks = new Array();
-	for(i=min; i<= max; i++)
-	{
-		ticks.push(""+i);
-	}
-        var name = "slider_"+(slider_uID++);
-	var selectHtml = "<select name='"+name+"' id='"+name+"' style='display:none'>";
-	for(var i in ticks)
-	{
-		selectHtml += "<option value='"+ticks[i]+"'>"+ticks[i]+"</option>";
-	}
-
-	selectHtml += "</select><br/><br/><a id='"+name+"_applybtn' href='#'>Apply</a>";
-
-	$("ul:last-child", div).after(selectHtml);
-
-	$("#"+name+"_applybtn", div.parent()).click(function(){
-            
-                var url = $(this).parent().find(".facet-list li:last a").attr("href");
-                var max = parseInt($(this).parent().find(".facet-list li:last a", div).text());
-		var value = parseInt($(this).parent().find("select option:selected").val());
-                url = url.replace(max, value);
-                url = url.replace((max+1), (value+1));
-		window.location = url;
-	});
-
-        $("ul", div).hide().parent().find("h4").css({marginBottom: '1em'});
-        $('#'+name).selectToUISlider().next();
 }
 
 function handleLoadError(obj)
 {
-    $(obj).addClass("loadError").html("Error");//.delay("slow").fadeOut("slow");
+	$(obj).addClass("loadError").html("Error");//.delay("slow").fadeOut("slow");
 }
 
 function hideDepartments()
 {
-    if ($(".alldept .selected").length > 0)
-    {
-        $(".popdept").hide();
-    }
-    else if ($(".popdept .selected").length > 0)
+	if ($(".alldept .selected").length > 0)
+	{
+		$(".popdept").hide();
+	}
+	else if ($(".popdept .selected").length > 0)
    {
-       var text = $("h3.facet-title:contains(popular)").parent().html();
-        $(".alldept form").hide().append(text);
-        $(".popdept  h3").hide();
+	   var text = $("h3.facet-title:contains(popular)").parent().html();
+		$(".alldept form").hide().append(text);
+		$(".popdept  h3").hide();
    }
 }
 
 function applyTabs()
 {
-    // hide all content
-    $(".tabbedcontent > div:gt(0)").hide();
-    
-    // enable action on each tab header
-    $("ul.tabs li a").click(function(){
-        $(this).parent().addClass("current").siblings(".current").removeClass("current");
-        var location = $(this).attr("href");
-        $(location).parent(".tabbedcontent").children().hide();
-        $(location).show(0, function(){ $(this).find(".fig").trigger("redraw")});
-        return false;
-    })
+	// hide all content
+	$(".tabbedcontent > div:gt(0)").hide();
+	
+	// enable action on each tab header
+	$("ul.tabs li a").click(function(){
+		$(this).parent().addClass("current").siblings(".current").removeClass("current");
+		var location = $(this).attr("href");
+		$(location).parent(".tabbedcontent").children().hide();
+		$(location).show(0, function(){ $(this).find(".fig").trigger("redraw")});
+		return false;
+	})
 }
 
 function renderImpacts()
 {
 
-    $('.show_impacts').unbind('click').click(function() {
-        $(this).toggleClass('open').blur();
+	$('.show_impacts').unbind('click').click(function() {
+		$(this).toggleClass('open').blur();
 
-        /* Switch the wording: */
-        $stem = $(this).text().substr(5);
-        if($(this).hasClass('open')) {
-            $(this).text('hide ' + $stem);
-        } else {
-            $(this).text('show ' + $stem);
-        }
+		/* Switch the wording: */
+		$stem = $(this).text().substr(5);
+		if($(this).hasClass('open')) {
+			$(this).text('hide ' + $stem);
+		} else {
+			$(this).text('show ' + $stem);
+		}
 
-        $(this).next().toggleClass('hide');
+		$(this).next().toggleClass('hide');
 		
-        return false;
-    });
+		return false;
+	});
+}
+
+function initLoadingIndicator()
+{
+	$('#loading').hide();
+	
+	window.onbeforeunload = function()  {
+		// add loading indicator after 2 second delay
+		$('#loading').delay(2000).show(0);
+
+		// allow user to cancel popup window
+		$(document).keyup(function(e)
+		{
+			if (e.keyCode == 27) { $('#loading').hide(); }   // esc
+		});
+	};
 }
